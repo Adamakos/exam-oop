f07c741d27cecb4acb4e44e26a031e61c2856e5a
Adamakos <adamhohmann@gmail.com>
2021-03-24 16:35:48 +0100

    Add files

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,44 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+import com.codecool.farm.animal.Cattle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
+
+    private final List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void feedAnimals() {
+        animals.forEach(Animal::feed);
+    }
+
+    public void butcher(Butcher butcher) {
+        this.animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return this.animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> statusList = new ArrayList<>();
+        String type;
+        for (Animal animal : this.animals) {
+            if (animal instanceof Cattle) type = "cattle";
+            else type = "pig";
+            statusList.add("There is a " + animal.getSize() + " sized " + type + " in the farm.");
+        }
+        return statusList;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
 
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,12 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    int size = 0;
+
+    public abstract void feed();
+
+    public int getSize() {
+        return size;
+    }
+
 }
